1b4771f41caee55d2863b1354bc07c1241b84986
YentlBelaen <yentl.belaen@student.vives.be>
2026-01-05 15:02:50 +0100

    Added Redux store and TanStack Query client providers

--- App.tsx
+++ App.tsx
@@ -1,13 +1,20 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Provider as ReduxProvider } from "react-redux";
+
 import RootTabNavigator from "./src/navigation/RootTabNavigator";
+import { queryClient } from "./src/lib/queryClient";
+import { store } from "./src/store/store";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <RootTabNavigator />
-    </NavigationContainer>
+    <ReduxProvider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <NavigationContainer>
+          <RootTabNavigator />
+        </NavigationContainer>
+      </QueryClientProvider>
+    </ReduxProvider>
   );
 }
 
